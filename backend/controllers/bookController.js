174ae16dd507aaf889a2ef1fd8e3a1c7bb8d6404
Bas15 <bolajistephen72@gmail.com>
2023-10-23 16:38:16 +0100

    add asycHandler to controllers

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,38 +1,40 @@
+const asyncHandler = require("express-async-handler")
+
 // @desc Get books
 // @route GET /api/books
 // @access public
-const getBooks = async (req, res) => {
+const getBooks = asyncHandler( async (req, res) => {
     res.status(200).json({ message : "get working"})
-}
+})
 
 
 // @desc Set books
 // @route POST /api/books
 // @access private
-const setBook = async (req, res) => {
+const setBook = asyncHandler(async (req, res) => {
     if(!req.body.text){
         res.status(400)
         throw new Error('please include text')
     }
 
     res.status(200).json({ message : "set working"})
-}
+})
 
 
 // @desc update books
 // @route PUT /api/books/:id
 // @access private
-const updateBook = async (req, res) => {
+const updateBook = asyncHandler(async (req, res) => {
     res.status(200).json({message : `put updated ${req.params.id}`})
-}
+})
 
 
 // @desc delete books
 // @route DELETE /api/books/:id
 // @access private
-const deleteBooks = async (req, res) => {
+const deleteBooks = asyncHandler(async (req, res) => {
     res.status(200).json({ message : `deleted ${req.params.id}`})
-}
+})
 
 module.exports = {
     getBooks, setBook, updateBook, deleteBooks
